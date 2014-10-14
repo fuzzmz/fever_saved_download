ab33a0bf51e37919427271998d7351e8130d48f5
Serban Constantin <serban.constantin@freescale.com>
2014-10-14 17:44:33 +0300

    read csv file and save links to vector

--- start.py
+++ start.py
@@ -1,5 +1,30 @@
 __author__ = 'Constantin Serban'
 
-# TODO open csv file
-# TODO read description column
+import optparse
+import csv
+
+
 # TODO download all .jpg images
+
+
+def get_links(f_obj):
+    # TODO read description column
+    pass
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--file',
+                      dest='csv_file',
+                      action='store',
+                      help='CSV file')
+
+    (opts, args) = parser.parse_args()
+    csv_file = opts.csv_file
+
+    with open(csv_file) as f_obj:
+        links = get_links(f_obj)
+
+
+if __name__ == "__main__":
+    main()
